b532e4d3cee594bd395a069dbb81c977626fb599
dlcastra <1dlcastra@gmail.com>
2023-09-25 16:45:35 +0200

    error correction

--- projecthillel/student_generation/views.py
+++ projecthillel/student_generation/views.py
@@ -13,7 +13,6 @@ def generation_student():
     birth_date = fake.date()
 
     student = CreateStudent(first_name=first_name, last_name=last_name, birth_date=birth_date)
-    # student = CreateStudent.objects.create(first_name=first_name, last_name=last_name, birth_date=birth_date)
     student.save()
 
 
@@ -26,14 +25,13 @@ def create_one_student(request):
 def create_multiply_students(request):
     try:
         count = int(request.GET.get('count', 0))
-
-        if count > 0 <= 100:
-            for _ in range(count):
-                generation_student()
-
-            return HttpResponse(f'Ви успішно знерували дані {count} студентів/та')
-
     except ValueError:
-        return HttpResponse('Для генераціі потрібно ввести число')
+        return HttpResponse('Для генерації потрібно ввести число')
 
-    return HttpResponse('Ви не можете згенерувати меньше одного або більше ніж 100 студентів')
+    if count <= 0 or 0 > 100:
+        return HttpResponse('Ви не можете згенерувати меньше одного або більше ніж 100 студентів')
+
+    for _ in range(count):
+        generation_student()
+
+    return HttpResponse(f'Ви успішно знерували дані {count} студентів/та')
