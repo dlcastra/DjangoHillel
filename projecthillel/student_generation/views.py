8636c98e22e1c77eb8ac683e53ab32c1c7bd64e5
dlcastra <1dlcastra@gmail.com>
2023-09-25 12:25:45 +0200

    have been amended

--- projecthillel/student_generation/views.py
+++ projecthillel/student_generation/views.py
@@ -1,40 +1,39 @@
 from django.http import HttpResponse
+
 from faker import Faker
 
 from .models import CreateStudent
 
-
 fake = Faker()
 
 
-def create_one_student(request):
+def generation_student():
     first_name = fake.first_name()
     last_name = fake.last_name()
     birth_date = fake.date()
 
     student = CreateStudent(first_name=first_name, last_name=last_name, birth_date=birth_date)
+    # student = CreateStudent.objects.create(first_name=first_name, last_name=last_name, birth_date=birth_date)
     student.save()
+
+
+def create_one_student(request):
+    generation_student()
 
     return HttpResponse('Ви успішно згенерували випадкові дані студента!')
 
 
 def create_multiply_students(request):
-
     try:
         count = int(request.GET.get('count', 0))
 
-        if count <= 100:
+        if count > 0 <= 100:
             for _ in range(count):
-                first_name = fake.first_name()
-                last_name = fake.last_name()
-                birth_date = fake.date()
-
-                student = CreateStudent(first_name=first_name, last_name=last_name, birth_date=birth_date)
-                student.save()
+                generation_student()
 
             return HttpResponse(f'Ви успішно знерували дані {count} студентів/та')
 
-        return HttpResponse('Нажаль Ви не можете згенерувати більше 100 студентів')
-
     except ValueError:
         return HttpResponse('Для генераціі потрібно ввести число')
+
+    return HttpResponse('Ви не можете згенерувати меньше одного або більше ніж 100 студентів')
