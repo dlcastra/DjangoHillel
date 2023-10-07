485c9b376adff4a532ca222d8175f0a27f8205c7
dlcastra <1dlcastra@gmail.com>
2023-10-07 12:45:36 +0200

    a teacher model and a group model were added

---
+++ projecthillel/school/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Teachers(models.Model):
+    first_name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    patronymic = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    subject = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name}, {self.surname}, {self.patronymic}, {self.birth_date}, {self.subject}"
+
+
+class Groups(models.Model):
+    group_name = models.CharField(max_length=100)
+    curator = models.ForeignKey(Teachers, on_delete=models.PROTECT)
