3ed197e2630a4a58e865f3f8f7c64c915704db9f
dlcastra <1dlcastra@gmail.com>
2023-10-07 12:49:16 +0200

    registered new app

--- projecthillel/projecthillel/settings.py
+++ projecthillel/projecthillel/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "student_generation.apps.StudentGenerationConfig",
     "teachers_generation.apps.TeachersGenerationConfig",
+    "school.apps.SchoolConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -56,7 +57,7 @@ ROOT_URLCONF = "projecthillel.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
