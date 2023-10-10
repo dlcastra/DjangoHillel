cc7e00b897f361bfdbdc3ae26b0ee372dfc972f8
dlcastra <1dlcastra@gmail.com>
2023-10-10 14:09:47 +0200

    add path for new functions

--- projecthillel/school/urls.py
+++ projecthillel/school/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path
 
 from school.views import (
-    teacher_form,
+    create_teacher,
     get_teachers_list,
+    edit_teacher,
+    delete_teacher,
     group_form,
     get_group_list,
-    main_page,
+    edit_group,
+    delete_group,
 )
 
 urlpatterns = [
-    path("teacher/", teacher_form, name="teacher_form"),
+    # Part for teachers
+    path("teacher/", create_teacher, name="teacher_form"),
     path("teachers/", get_teachers_list, name="get_teachers_list"),
+    path("edit-teacher/<int:pk>", edit_teacher, name="edit_teacher"),
+    path("delete-teacher/<int:pk>", delete_teacher, name="delete_teacher"),
+    # Part for groups
     path("group/", group_form, name="group_form"),
     path("groups/", get_group_list, name="get_group_list"),
+    path("edit-group/<int:pk>", edit_group, name="edit_group"),
+    path("delete-group/<int:pk>", delete_group, name="delete_group"),
 ]
