ddcc48200efe4fe9ac31f75ce543a118163d22a1
dlcastra <1dlcastra@gmail.com>
2023-10-10 14:07:27 +0200

    changed the form TeachersForm

--- projecthillel/school/forms.py
+++ projecthillel/school/forms.py
@@ -1,14 +1,19 @@
 from django import forms
 
-from school.models import Groups
+from school.models import Teachers, Groups
 
 
-class TeachersForm(forms.Form):
-    first_name = forms.CharField(label="Ім'я")
-    surname = forms.CharField(label="Прізвище")
-    patronymic = forms.CharField(label="По батькові")
-    birth_date = forms.DateField(label="Дата народження")
-    subject = forms.CharField(label="Назва предмету")
+class TeachersForm(forms.ModelForm):
+    class Meta:
+        model = Teachers
+        fields = ["first_name", "surname", "patronymic", "birth_date", "subject"]
+        labels = {
+            "first_name": "Ім'я",
+            "surname": "Прізвище",
+            "patronymic": "По батькові",
+            "birth_date": "Дата народження",
+            "subject": "Назва предмету",
+        }
 
     def clean_first_name(self):
         first_name = self.cleaned_data["first_name"]
@@ -20,6 +25,7 @@ class TeachersForm(forms.Form):
             raise forms.ValidationError(
                 "The name must not contain any numbers/Ім'я не повинно мати цифр"
             )
+        return first_name
 
     def clean_surname(self):
         surname = self.cleaned_data["surname"]
@@ -31,6 +37,7 @@ class TeachersForm(forms.Form):
             raise forms.ValidationError(
                 "The surname must not contain any numbers/Прізвище не повинно мати цифр"
             )
+        return surname
 
     def clean_patronymic(self):
         patronymic = self.cleaned_data["patronymic"]
@@ -42,6 +49,7 @@ class TeachersForm(forms.Form):
             raise forms.ValidationError(
                 "The patronymic must not contain any numbers/Прізвище по батькові не повинно мати цифр"
             )
+        return patronymic
 
     def clean_subject(self):
         subject = self.cleaned_data["subject"]
@@ -49,9 +57,22 @@ class TeachersForm(forms.Form):
             raise forms.ValidationError(
                 "Subject name is too long, max length is 100/Назва предмету занадто велика, максимальна довжина 100"
             )
+        return subject
 
 
 class GroupsForm(forms.ModelForm):
     class Meta:
         model = Groups
         fields = ["group_name", "curator"]
+        labels = {
+            "group_name": "Назва групи",
+            "curator": "Куратор групи",
+        }
+
+    def clean_group_name(self):
+        group_name = self.cleaned_data["group_name"]
+        if len(group_name) > 100:
+            raise forms.ValidationError(
+                "Group name is too long, max length is 100/Назва групи занадто велика, максимальна довжина 100"
+            )
+        return group_name
