6dad5b16e3dfa73b342cff60b0d45439bac9ebb3
dlcastra <1dlcastra@gmail.com>
2023-10-11 12:46:38 +0200

    add m2m field

--- projecthillel/student_generation/models.py
+++ projecthillel/student_generation/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from school.models import Groups
+
 
 class CreateStudent(models.Model):
     student_id = models.AutoField(primary_key=True)
@@ -7,3 +9,7 @@ class CreateStudent(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
+    group = models.ManyToManyField(Groups, related_name="group")
+
+    def __str__(self):
+        return f"{self.first_name, self.first_name}"
