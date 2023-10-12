67cec534ac8a31b16e21ea92f1b1c61644b510c2
dlcastra <1dlcastra@gmail.com>
2023-10-12 12:18:25 +0200

    change model name

--- projecthillel/teachers_generation/models.py
+++ projecthillel/teachers_generation/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Teacher(models.Model):
+class GenerateTeacher(models.Model):
     teacher_id = models.AutoField(primary_key=True)
 
     first_name = models.CharField(max_length=50)
