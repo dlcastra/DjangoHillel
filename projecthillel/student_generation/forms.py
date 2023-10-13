8506489ee712925459c9558e639a5908faa1c27b
dlcastra <1dlcastra@gmail.com>
2023-10-13 14:09:39 +0200

    added clean method for a new value in the model

--- projecthillel/student_generation/forms.py
+++ projecthillel/student_generation/forms.py
@@ -1,16 +1,19 @@
 from django import forms
 
+import phonenumbers
+
 from student_generation.models import CreateStudent
 
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = CreateStudent
-        fields = ["first_name", "last_name", "birth_date"]
+        fields = ["first_name", "last_name", "birth_date", "phone"]
         labels = {
             "first_name": "Ім'я",
             "last_name": "Прізвище",
             "birth_date": "Дата народження",
+            "phone": "Номер телефону",
         }
 
     def clean_first_name(self):
@@ -37,6 +40,30 @@ class StudentForm(forms.ModelForm):
                 "The last name must not contain any numbers/Прізвище не повинно мати цифр"
             )
         return last_name
+
+    def clean_phone(self):
+        phone = self.cleaned_data["phone"]
+        if not phone:
+            raise forms.ValidationError(
+                "Phone cannot be empty/Поле телефон не може бути пустим"
+            )
+
+        if len(phone) < 9:
+            raise forms.ValidationError(
+                "The number provided is too short/Наданий номер занадто короткий"
+            )
+
+        try:
+            parsed = phonenumbers.parse(phone, None)
+        except phonenumbers.NumberParseException as error:
+            raise forms.ValidationError(
+                f"{error.args[0]}/Наданий рядок, схоже, не був номером телефону "
+            )
+
+        formatted_phone = phonenumbers.format_number(
+            parsed, phonenumbers.PhoneNumberFormat.INTERNATIONAL
+        )
+        return formatted_phone
 
 
 class AddToGroup(forms.ModelForm):
